d173229a0b4bef475d4a57353301a96b2af0fa1a
Magic <magic753@hotmail.com>
2021-08-14 21:04:12 -0500

    backend servicio getuser

--- backend/src/controllers/usuarioController.js
+++ backend/src/controllers/usuarioController.js
@@ -2,49 +2,31 @@ const { errorHandler } = require('../helpers/dbErrorHandler');
 const formidable = require('formidable');
 const _ = require('lodash');
 const fs = require('fs');
+const { addUsuario, findUsuarioById } = require('../dao/usuarioDAO');
 //Model
 const Usuario = require('../models/Usuario');
 
 exports.create = async (req, res) => {
-    let form = new formidable.IncomingForm()
-    form.keepExtensions = true
-    form.parse(req, async (err, fields, files) => { 
-        if (err) {
-            return res.status(400).json({
-                error: "Error al leer datos"
-            })
-        }
-        const { usuario, 
-                contrasenia,
-                correo,
-                rol } = fields;
-        const nuevoUsuario = new Usuario(fields);
-        if (files.avatar) {
-            if (files.avatar.size > 1000000) {
-              return res.status(400).json({
-                error: "La imagen debe pesar menos de 1MB"
-              })
-            }
-            nuevoUsuario.avatar.data = fs.readFileSync(files.avatar.path)
-            nuevoUsuario.avatar.contentType = files.avatar.type
-        }
-        await nuevoUsuario.save((err, data) => {
-            if(err){
-                return res.status(400).json({
-                    error: errorHandler(err),
-                    status: 0,
-                    msg: "Error al insertar datos"
-                })
-            }
-            res.json({
-                status: 1,
-                msg: "Insertado correctamente"
-            });
-        });
-    })
+    let data = req.body;
+    let foto_data = null;
+    let foto_tipo = null;
+    if (data.foto) {
+      foto_data = Buffer.from(data.foto, "base64");
+      foto_tipo = data.foto_tipo;
+      if (foto_data > 100000) {
+        respuesta.err.push("La imagen debe pesar menos de 1MB");
+        foto_data = null;
+        foto_tipo = null;
+      }
+    }
+    let confirm = await addUsuario(data.usuario, data.correo, await bcrypt.hash(data.contrasenia, saltRounds), rol.id, foto_data, foto_tipo);
+    if(confirm.error){
+        return res.status(400).json(confirm);
+    }
+    return res.json(confirm);
 }
 
-exports.read = async (req, res) => {
+exports.read = async (req, res) => { //falta modificar DAO
     let order = req.query.order ? req.query.order : 'asc';
     let sortBy = req.query.sortBy ? req.query.sortBy : 'name'
     await Usuario.find()
@@ -64,20 +46,15 @@ exports.read = async (req, res) => {
 }
 
 exports.readById = async (req, res) => {
-    await Usuario.findById(req.params.id)
-        .exec((err, data) => {
-            if(err){
-                return res.status(400).json({
-                    error: errorHandler(err),
-                    status: 0,
-                    msg: "Error al encontrar objeto"
-                })
-            }
-            res.json(data);
-        });
+    const {  id  } = req.query;
+    let data = await findUsuarioById(id);
+    if(data.error){
+        return res.status(400).json(data);
+    }
+    return res.json(data);
 }
 
-exports.avatar = async (req, res) => {
+exports.avatar = async (req, res) => {//falta modificar DAO
     if(req.usuario.avatar.data){
         await res.set('Content-Type', req.usuario.avatar.contentType)
         return res.send({
@@ -88,7 +65,7 @@ exports.avatar = async (req, res) => {
 }
 
 
-exports.update = async (req, res) => {
+exports.update = async (req, res) => { //falta modificar DAO
     let form = new formidable.IncomingForm()
     form.keepExtensions = true
     form.parse(req, async (err, fields, files) => {  
@@ -106,19 +83,6 @@ exports.update = async (req, res) => {
     
 }
 
+exports.remove = () =>{ //falta modificar DAO
 
-exports.objectById = async (req, res, next, id) => {
-    Usuario.findById(id).exec((err, data) => {
-        if(err || !data){
-            return res.status(400).json({
-                error: err,
-                status: 0,
-                msg: "No se encontró el objeto"
-            })
-        }
-        req.Usuario = data;
-        next();
-    })
-}
-
-exports.remove = () =>{}
+} 
